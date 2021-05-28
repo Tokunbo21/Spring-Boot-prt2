6fc0825c840fdce2bfa7d98d5861586fa0433fe3
unknown <tofadetokunbo@hotmail.com>
2021-05-28 20:02:02 -0300

    28/05

--- brogPessoal/src/main/java/org/Generation/brogPessoal/repository/PostagemRepository.java
+++ brogPessoal/src/main/java/org/Generation/brogPessoal/repository/PostagemRepository.java
@@ -2,6 +2,7 @@ package org.Generation.brogPessoal.repository;
 
 import java.util.List;
 
+
 import org.Generation.brogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
 	
 }
