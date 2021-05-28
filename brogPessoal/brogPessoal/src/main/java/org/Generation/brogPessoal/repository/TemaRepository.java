7aa5cc5829320fd8c24abb645375a316464fc05d
unknown <tofadetokunbo@hotmail.com>
2021-05-28 19:53:14 -0300

    28/05

--- brogPessoal/brogPessoal/src/main/java/org/Generation/brogPessoal/repository/TemaRepository.java
+++ brogPessoal/brogPessoal/src/main/java/org/Generation/brogPessoal/repository/TemaRepository.java
@@ -2,13 +2,15 @@ package org.Generation.brogPessoal.repository;
 
 import java.util.List;
 
-import org.Generation.brogPessoal.model.Postagem;
+import org.Generation.brogPessoal.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
-	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+public interface TemaRepository extends JpaRepository<Tema, Long >{
 	
+	public List<Tema>findAllByDescricaoContainingIgnoreCase(String descricao);
+
+	
+
 }
---
+++ brogPessoal/brogPessoal/src/test/java/org/Generation/brogPessoal/BrogPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Generation.brogPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrogPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
